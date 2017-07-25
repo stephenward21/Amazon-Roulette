ec6747c15481b47240170941c16076e17c763c67
Shane Hall <cyrus.s.hall84@gmail.com>
2017-07-25 12:00:28 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <h1>00000000</h1>
-          <h1>00000000</h1>
-          <h1>00000000</h1>
-          <h1>00000000</h1>
-          <h1>00000000</h1>
+        <Navbar/>
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import $ from 'jquery';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+
+
+class NavBar extends Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(
+			<div>
+				<Navbar inverse collapseOnSelect fixedTop>
+					<Navbar.Header>
+						<Navbar.Brand>
+							site logo here
+						</Navbar.Brand>
+                        <Navbar.Toggle />
+					</Navbar.Header>
+					<Navbar.Collapse>
+						<Nav pullRight>
+							<NavItem eventKey={1}>
+								About
+							</NavItem>
+                            <NavItem eventKey={2}>
+								Contact
+							</NavItem>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+			</div>
+		)
+	}
+}
+
+
+export default NavBar
