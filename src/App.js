d26fe4357d4c1be4218d75f20dbccac60fb6eff8
Shane Hall <cyrus.s.hall84@gmail.com>
2017-07-25 10:21:24 -0400

    text

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
           <h1>00000000</h1>
+          <h1>00000000</h1>
       </div>
     );
   }
