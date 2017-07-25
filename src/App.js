e9fd5f0129dbdd2d5ccc51d2abc783fa38207dff
Shane Hall <cyrus.s.hall84@gmail.com>
2017-07-25 12:10:43 -0400

    register page and navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Navbar from './components/Navbar';
-=======
-import Home from './components/Home';
-import Register from './components/Register';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
->>>>>>> 0d56f739619e46afd3c31388fab5d31a0f4650a6
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Register from './components/Register'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
 
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <Navbar/>
-      </div>
-=======
       <Router>
         <div className="App">
           <div id="canvas">
+            <Navbar/>
             <Home />
             <Route exact path="/register" component={Register} />
           </div>
         </div>
       </Router>
->>>>>>> 0d56f739619e46afd3c31388fab5d31a0f4650a6
     );
   }
 }
