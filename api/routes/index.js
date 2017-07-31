7bad5ffb59a03925d288c2f35ecf581417d4c572
Shane Hall <cyrus.s.hall84@gmail.com>
2017-07-30 23:31:45 -0400

    created db connection, config file

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
