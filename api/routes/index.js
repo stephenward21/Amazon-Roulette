6db5a70cdf353ba9a360d98a3a5d24f19ca1e6ae
Shane Hall <cyrus.s.hall84@gmail.com>
2017-07-31 10:13:31 -0400

    config and db

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var config = require('../config/config');
+var amazon = require('amazon-product-api')
+
 
 var connection = mysql.createConnection({
     host: config.host,
     user: config.user,
     password: config.password,
     database: config.database
+});
+
+
+var client = amazon.createClient({
+ awsId: config.awsId,
+ awsSecret: config.awsSecret,
+ awsTag: config.awsTag
+
 });
 
 connection.connect();
